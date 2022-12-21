845ef41dd10b03034ea95798b6a74c19ce94dc9f
JRisJunior <jeffmrubin@gmail.com>
2022-12-21 13:52:46 -0600

    add create and new actions and new view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,20 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: params[:id])
     render template: "pets/show"
   end
+
+  def new
+    @pet = Pet.new
+    render template: "pets/new"
+  end
+
+  def create
+    @pet = Pet.new(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    @pet.save
+    redirect_to "/pets"
+  end
+
 end
