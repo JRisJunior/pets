3dff1efb4dcfb7d782f6f937ca40bebac8d5b567
JRisJunior <jeffmrubin@gmail.com>
2022-12-21 14:20:04 -0600

    add edit and destroy actions + strict code create + add bcrypt gem

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,13 +15,33 @@ class PetsController < ApplicationController
   end
 
   def create
-    @pet = Pet.new(
-      name: params[:pet][:name],
-      breed: params[:pet][:breed],
-      image: params[:pet][:image],
-    )
+    @pet = Pet.new(pet_params)
     @pet.save
     redirect_to "/pets"
   end
 
+  def edit
+    @pet = Pet.find_by(id: params[:id])
+    render template: "pets/edit"
+  end
+
+  def update
+    @pet = Pet.find_by(id: params[:id])
+    @pet.name = params[:pet][:name]
+    @pet.breed = params[:pet][:breed]
+    @pet.image = params[:pet][:image]
+    @pet.save
+    redirect_to "/pets"
+  end
+
+  def destroy
+    @pet = Pet.find_by(id: params[:id])
+    @pet.destroy
+    redirect_to "/pets", status: :see_other
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :image)
+  end
 end
